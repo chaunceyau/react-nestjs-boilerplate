a85bcc0fd60e1f32b54402735c55a5cbc846e05d
Austin Chauncey <chauncey.au@gmail.com>
2020-05-19 10:31:49 -0700

    feature: update client navigation bar

--- client/src/modules/navigation/navigation.tsx
+++ client/src/modules/navigation/navigation.tsx
@@ -9,8 +9,48 @@ export interface INavigationProps {
 
 export function Navigation(props: INavigationProps) {
   return (
-    <div>
-      <h1>Boilerplate :)</h1>
-    </div>
+    <nav className="flex items-center justify-between flex-wrap bg-red-600 px-6 py-4">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight">
+          🚀 Boilerplate
+        </span>
+      </div>
+      <div className="block lg:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-red-200 border-red-400 hover:text-white hover:border-white">
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-red-200 hover:text-white mr-4"
+          >
+            Dashboard
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-red-200 hover:text-white mr-4"
+          >
+            Examples
+          </a>
+        </div>
+        <div>
+          <a
+            href="#"
+            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-red-500 hover:bg-white mt-4 lg:mt-0"
+          >
+            My Account
+          </a>
+        </div>
+      </div>
+    </nav>
   )
 }
